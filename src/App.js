0468a7c8069f315142b0ad91c25550ca5a14e808
Angel Gonzalez <aangel.gg@hotmail.com>
2024-02-28 18:54:24 +0100

    Configure Eslint and Prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'></header>
+		</div>
+	);
 }
 
 export default App;
